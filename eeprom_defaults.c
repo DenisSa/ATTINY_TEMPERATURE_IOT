1d962677581302b0c321a11617395b69029726bb
Denis <denis.saz@gmail.com>
2017-08-13 01:21:09 -0400

    Initialize deviceid to 1 to work around issue with reading 0 from eeprom

--- eeprom_defaults.c
+++ eeprom_defaults.c
@@ -1,6 +1,6 @@
 #include <avr/eeprom.h>
 
-uint8_t EEMEM dev_id = 0;
+uint8_t EEMEM dev_id = 1;
 uint32_t EEMEM ip_addr = 0xC0A80067; //192.168.0.103
 uint16_t EEMEM ip_port = 5555;
 uint8_t EEMEM wifi_username[32];
